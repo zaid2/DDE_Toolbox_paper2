c90e5fb73b7aee0aaca0375c6ee3f148bbc61ea4
zaid2 <zaid2@illinois.edu>
2022-04-20 16:44:24 -0500

    Update ddaecoll_init_sol.m

--- Toolbox_DDE/ddaecoll/ddaecoll_init_sol.m
+++ Toolbox_DDE/ddaecoll/ddaecoll_init_sol.m
@@ -1,18 +1,5 @@
 function sol = ddaecoll_init_sol(data, t0, x0, y0, p0)
-%COLL_INIT_SOL   Build initial solution guess.
-%
-% Use sampled trajectory on temporal mesh to construct initial solution
-% guess for 'coll' toolbox.
-%
-% SOL = COLL_INIT_SOL(DATA, T0, X0, P0)
-%
-% DATA - Toolbox data structure.
-% T0   - Array of temporal mesh points.
-% X0   - Array of state vectors at mesh points.
-% P0   - Initial solution guess for problem parameters.
-
-% Copyright (C) Frank Schilder, Harry Dankowicz
-% $Id: coll_init_sol.m 2839 2015-03-05 17:09:01Z fschild $
+% Copyright (C) Zaid Ahsan, Mingwu Li
 
 t0 = t0(:);
 T0 = t0(1);
