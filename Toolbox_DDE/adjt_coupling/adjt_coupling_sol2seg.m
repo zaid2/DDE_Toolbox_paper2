fe29fab625bf319d9de7cf58b8531c5878d9d4ed
zaid2 <zaid2@illinois.edu>
2022-04-20 16:29:52 -0500

    Update adjt_coupling_sol2seg.m

--- Toolbox_DDE/adjt_coupling/adjt_coupling_sol2seg.m
+++ Toolbox_DDE/adjt_coupling/adjt_coupling_sol2seg.m
@@ -1,34 +1,6 @@
 function prob = adjt_coupling_sol2seg(prob, oid, W, varargin)
-%ADJT_COLL2COLL   Append adjoint of 'coll' instance from saved solution.
-%
-% PROB = ADJT_COLL2COLL(PROB, OID, VARARGIN)
-% VARARGIN = { RUN [SOID] LAB }
-%
-% Append adjoint of a 'coll' instance with object instance identifier OID
-% that has been previously added to the continuation problem contained in
-% PROB from the same saved solution using ODE_COLL2COLL.
-%
-% PROB : Continuation problem structure.
-% OID  : Target object instance identifier (string). Pass the empty string
-%        '' for a simple continuation of trajectory segments.
-%
-% See ODE_ISOL2COLL for more details on PROB and OID.
-%
-% RUN  : Run identifier (string or cell-array of strings). Name of the run
-%        from which to restart a new continuation run.
-% SOID : Source object instance identifier (string, optional). If the
-%        argument SOID is omitted, OID is used. Pass the empty string ''
-%        for OID and omit SOID for a simple continuation of trajectory
-%        segments. Pass non-trivial object identifiers if an instance of
-%        the COLL toolbox is part of a composite continuation problem.
-% LAB  : Solution label (integer). The integer label assigned by COCO to an
-%        trajectory segment during the continuation run RUN.
-%
-% See also: ADJT_ISOL2COLL, ADJT_BP2COLL, COLL_READ_ADJOINT,
-% COLL_ADJT_INIT_DATA, COLL_CONSTRUCT_ADJT
 
-% Copyright (C) Frank Schilder, Harry Dankowicz, Mingwu Li
-% $Id: ode_coll2coll.m 2898 2015-10-07 21:17:13Z hdankowicz $
+% Copyright (C) Zaid Ahsan
 
 grammar   = 'RUN [SOID] LAB [POINT] [OPTS]';
 args_spec = {
