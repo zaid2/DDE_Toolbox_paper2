fd96e7c4188ac135678acfe1a1d763f957469db1
zaid2 <zaid2@illinois.edu>
2022-04-20 16:28:49 -0500

    Update adjt_coupling_isol2seg.m

--- Toolbox_DDE/adjt_coupling/adjt_coupling_isol2seg.m
+++ Toolbox_DDE/adjt_coupling/adjt_coupling_isol2seg.m
@@ -1,35 +1,6 @@
 function prob = adjt_coupling_isol2seg(prob, oid, W, varargin)
-%ADJT_ISOL2COLL   Append adjoint of 'coll' instance from initial guess.
-%
-% PROB = ADJT_ISOL2COLL(PROB, OID, VARARGIN)
-% VARARGIN = { [OPTS] }
-% OPTS = { '-coll-end' | '-end-coll' }
-%
-% Append adjoint of a 'coll' instance with object instance identifier OID
-% that has been previously added to the continuation problem contained in
-% PROB using ODE_ISOL2COLL. The preceding call to ODE_ISOL2COLL must
-% include explicit Jacobians, while functions evaluating second derivatives
-% are optional. 
-%
-% On input:
-%
-% PROB   : Continuation problem structure.
-%
-% OID    : Object instance identifier (string). The corresponding toolbox
-%          instance identifier is coco_get_id(OID, 'coll'). Pass the empty
-%          string '' for a simple continuation of trajectory segments. Pass
-%          a non-trivial object identifier if an instance of the COLL
-%          toolbox is part of a composite continuation problem.
-%
-% OPTS   : '-coll-end' and '-end-coll' (optional, multiple options may be
-%          given). Either '-coll-end' or '-end-coll' marks the end of input
-%          to ADJT_ISOL2COLL.
-%
-% See also: ODE_ISOL2COLL, COLL_READ_ADJOINT, COLL_ADJT_INIT_DATA,
-% COLL_CONSTRUCT_ADJT
 
-% Copyright (C) Frank Schilder, Harry Dankowicz, Mingwu Li
-% $Id: ode_isol2coll.m 2898 2015-10-07 21:17:13Z hdankowicz $
+% Copyright (C) Zaid Ahsan
 
 if isempty(W)
 tbid = coco_get_id(oid, 'coupling'); % Create toolbox instance identifier
