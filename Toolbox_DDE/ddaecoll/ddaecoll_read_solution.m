d3eb38fb5e54eba1699812af9e8e82649e922dd2
zaid2 <zaid2@illinois.edu>
2022-04-20 16:44:57 -0500

    Update ddaecoll_read_solution.m

--- Toolbox_DDE/ddaecoll/ddaecoll_read_solution.m
+++ Toolbox_DDE/ddaecoll/ddaecoll_read_solution.m
@@ -1,19 +1,5 @@
 function [sol data] = ddaecoll_read_solution(oid, run, lab)
-%COLL_READ_SOLUTION   Read 'coll' solution and toolbox data from disk.
-%
-% Extract data and chart structures associated with 'coll' toolbox instance
-% identifier from solution file and construct solution structure.
-%
-% [SOL DATA] = COLL_READ_SOLUTION(OID, RUN, LAB)
-%
-% SOL  - Solution (struct).
-% DATA - Toolbox data (struct).
-% OID  - Object instance identifier (string).
-% RUN  - Run identifier (string).
-% LAB  - Solution label (integer).
-
-% Copyright (C) Frank Schilder, Harry Dankowicz
-% $Id: coll_read_solution.m 2839 2015-03-05 17:09:01Z fschild $
+% Copyright (C) Zaid Ahsan, Mingwu Li
 
 tbid         = coco_get_id(oid, 'ddaecoll');
 [data chart] = coco_read_solution(tbid, run, lab);
