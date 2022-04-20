319ff98f171c9bad9d93e8e713644a5ab6b71193
zaid2 <zaid2@illinois.edu>
2022-04-20 16:45:15 -0500

    Update ddaecoll_sol2seg.m

--- Toolbox_DDE/ddaecoll/ddaecoll_sol2seg.m
+++ Toolbox_DDE/ddaecoll/ddaecoll_sol2seg.m
@@ -1,20 +1,5 @@
 function prob = ddaecoll_sol2seg(prob, oid, varargin)
-%COLL_SOL2SEG   Append 'coll' instance constructed from saved data.
-%
-% Support restarting continuation from a previously obtained solution,
-% stored to disk.
-%
-% PROB     = COLL_SOL2SEG(PROB, OID, VARARGIN)
-% VARARGIN = { RUN [SOID] LAB }
-%
-% PROB - Continuation problem structure.
-% OID  - Target object instance identifier (string).
-% RUN  - Run identifier (string).
-% SOID - Source object instance identifier (string).
-% LAB  - Solution label (integer).
-
-% Copyright (C) Frank Schilder, Harry Dankowicz
-% $Id: coll_sol2seg.m 2839 2015-03-05 17:09:01Z fschild $
+% Copyright (C) Zaid Ahsan, Mingwu Li
 
 tbid = coco_get_id(oid, 'ddaecoll'); % Create toolbox instance identifier
 str  = coco_stream(varargin{:}); % Convert varargin to stream of tokens for argument parsing
